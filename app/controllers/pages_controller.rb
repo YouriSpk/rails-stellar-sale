5137fdb003149e03df0f5714a2776979a8debb1f
Medard Kuklis <medocius@gmail.com>
2023-11-29 14:24:20 +0100

    added home, new, index

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
   def home
+    @stars = Star.limit(10)
   end
 end
--- app/controllers/stars_controller.rb
+++ app/controllers/stars_controller.rb
@@ -1,8 +1,32 @@
 class StarsController < ApplicationController
+
   def show
     @star = Star.find(params[:id])
   end
+
   def index
     @stars = Star.all
   end
+
+  def new
+    @star = Star.new
+  end
+
+  def create
+    @star = Star.new(star_params)
+    @star.user_id = 1
+    if @star.save
+      redirect_to stars_path, notice: 'Star was successfully created.'
+    else
+      flash.now[:alert] = 'Failed to create star. Please fix the errors below.'
+      render :new
+    end
+  end
+
+  private
+
+  def star_params
+    params.require(:star).permit(:initial_name, :price, :category, :distance_from_earth_in_light_years, :spectral_class)
+  end
+
 end

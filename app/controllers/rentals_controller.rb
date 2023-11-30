17fee5d11ded2c270e5ce1c9c7496833302d7cf6
Basant Morsi <basant179@gmail.com>
2023-11-30 15:27:23 +0100

    datepickr

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -5,7 +5,12 @@ class RentalsController < ApplicationController
   end
 
   def create
+    start_date = params[:rental][:start_date].split(",")[0]
+    end_date =  params[:rental][:start_date].split(",")[1]
+    difference = (Date.parse(end_date) - Date.parse(start_date)).to_i
     @rental = Rental.new(rental_param)
+    @rental.start_date = start_date
+    @rental.end_date = end_date
     @star = Star.find(params[:star_id])
     @rental.star = @star
     @rental.user_id = current_user.id
@@ -19,6 +24,6 @@ class RentalsController < ApplicationController
   private
 
   def rental_param
-    params.require(:rental).permit(:new_name, :start_date, :end_date)
+    params.require(:rental).permit(:new_name)
   end
 end

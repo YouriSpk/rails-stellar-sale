f6ce64fbee9a43e08ba5a95bee58d070614ed9cd
Basant Morsi <basant179@gmail.com>
2023-11-30 16:09:42 +0100

    calculate sum

--- app/javascript/controllers/datepicker_controller.js
+++ app/javascript/controllers/datepicker_controller.js
@@ -3,6 +3,7 @@ import flatpickr from "flatpickr";
 
 // Connects to data-controller="datepicker"
 export default class extends Controller {
+  static targets = ["totalSum"]
   connect() {
     flatpickr(this.element, {
       minDate: "today",
@@ -11,6 +12,12 @@ export default class extends Controller {
         if (selectedDates.length === 2) {
           const startDate = selectedDates[0];
           const endDate = selectedDates[1];
+          const date1 = new Date(startDate);
+          const date2 = new Date(endDate);
+          const diffTime = Math.abs(date2 - date1);
+          const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+
+          document.querySelector("#totalSum").innerHTML =(parseInt(document.querySelector("#starPrice").innerText) * diffDays) + " Euro"
         }
     }
     })

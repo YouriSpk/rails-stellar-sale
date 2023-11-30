e104fea9995174ad71c7d29d54c533dc0b1ea292
Basant Morsi <basant179@gmail.com>
2023-11-30 12:37:18 +0100

    toggle function

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -2,6 +2,11 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="toggle"
 export default class extends Controller {
+  static targets = ["togglableElement"]
   connect() {
+
+  }
+  fire() {
+    this.togglableElementTarget.classList.toggle("d-none");
   }
 }
